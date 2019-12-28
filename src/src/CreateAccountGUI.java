7ef4c63b5c66bc89025eb7b9ee6415b2f82e5f6a
karolhusagh <huuusiq@gmail.com>
2019-12-28 17:37:47 +0100

    asdasdas

---
+++ src/src/CreateAccountGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class CreateAccountGUI {
+    private JTextField AccNum;
+    private JTextField Name;
+    private JTextField Address;
+    private JButton closeButton;
+    private JButton createAccountButton;
+    private JTextField textField1;
+}
---
+++ src/src/TransferGui.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class TransferGui {
+    private JPanel panel1;
+    private JButton saveButton;
+    private JButton closeButton;
+    private JButton resetButton;
+    private JTextField RemBal1;
+    private JTextField RemBal2;
+    private JComboBox AccNum1;
+    private JComboBox Accnum2;
+    private JTextField Balance2;
+    private JTextField Balance1;
+    private JTextField textField5;
+    private JButton getTotalButton;
+}
